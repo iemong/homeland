36391bf7e5f464bf5e876cc97a00ef2e829a4643
入江信之介 <(kiyoshidainagon@gmail.com)>
2016-01-02 01:14:12 +0900

    修正

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ var gulp = require('gulp'),
 			phpTarget  : 'assets/**/*.php',
 			// css, Sass(scss)
 			scssSrc    : 'assets/scss/**.scss',
-            scssMain   : 'assets/scss/top.scss',
+            scssMain   : 'assets/scss/news.scss',
 			scssDest   : 'build/css',
 			scssTarget : 'assets/scss/**/*.scss',
 			// javascript
